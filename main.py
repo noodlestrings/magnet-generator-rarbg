f9a54192a0e8044773953147bb298b7f8d9bcb09
Sam Lee <sammystradling2006@gmail.com>
2023-06-03 14:53:20 +0100

    progress, basic functionality

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import sqlite3
 
-conn = sqlite3.connect('rarbg_db.sqlite')
 
-# Create a cursor object
-cursor = conn.cursor()
+def fetch_data(searchTerm, cursor):
+    searchTerm = "%" + searchTerm + "%"
+    cursor.execute("SELECT * FROM items WHERE title LIKE ? AND cat LIKE ?", (searchTerm, 'movie%'))
+    rows = cursor.fetchall()
+    return rows
 
-# Fetch data
-searchTerm = "%monty%"
-cursor.execute("SELECT * FROM items WHERE title LIKE ?", (searchTerm))
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-
+def clean_data(data):
+    # cleans the data from the database
+    pass 
 
 
-# Close the connection
-conn.close()
+def init_db(filename):
+    conn = sqlite3.connect(filename)
+    # Create a cursor object
+    cursor = conn.cursor()
+    return (cursor, conn)
+
+def magnetiser(hash, name):
+    pass
+
+
+def main():
+    cursor, conn = init_db("rarbg_db.sqlite")
+    searchTerm = input("Enter a search term: ")
+    data=clean_data(fetch_data(searchTerm, cursor))
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
