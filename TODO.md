cd4af68c1ce2ab0a26badbc5cc13811b00f1b321
Sam Lee <sammystradling2006@gmail.com>
2023-06-04 21:27:08 +0100

    searching with specific filters now works, see TODO for what has been done and what needs to be done

--- TODO.md
+++ TODO.md
@@ -4,7 +4,8 @@
 - [x] update fetch_data() to also search using a period delimited string
 - [x] write clean_data() function that cleans the data
 - [x] create an input_params() function to search for the movie, with given parameters such as resolution
-- [ ] update fetch_data() to use the input parameters
+- [x] update fetch_data() to use the input parameters
+- [ ] display to the user the options they have, and allow them to enter the id they want
 - [ ] write magnetiser() to generate the magnet link
 
 ## Notes
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import sqlite3
 
+
 def init_db(filename):
     conn = sqlite3.connect(filename)
     # Create a cursor object
     cursor = conn.cursor()
     return (cursor, conn)
 
+
 def get_filter_parameters():
     filterParameters = {}
-    print("Please enter 'True' or 'False' for each option")
-    showOnly4k = input("Only see 4k torrents: ").lower() == "true"
-    show720p = input("See 720p torrents: ").lower() == "true"
-    show480p = input("See 480p torrents: ").lower() == "true"
-    showXvidFormat = input("See xvid format torrents: ").lower() == "true"
+    print("Please only enter 't' or 'f' for each option")
+    showOnly4k = input("Only see 4k torrents: ").lower()[0] == "t"
+    show720p = input("See 720p torrents: ").lower()[0] == "t"
+    show480p = input("See 480p torrents: ").lower()[0] == "t"
+    showXvidFormat = input("See xvid format torrents: ").lower()[0] == "t"
 
     filterParameters["show-only-4k"] = showOnly4k
     filterParameters["show-720p"] = show720p
@@ -21,23 +23,26 @@ def get_filter_parameters():
 
     return filterParameters
 
-def fetch_data(searchTerm, cursor, **filters):
+
+def fetch_data(searchTerm, cursor):
     searchTerm = "%" + searchTerm + "%"
     cursor.execute(
-        "SELECT * FROM items WHERE title LIKE ? OR title LIKE ? AND cat LIKE ?", (searchTerm, searchTerm.replace(" ", "."), "movie%")
+        "SELECT * FROM items WHERE title LIKE ? OR title LIKE ? AND cat LIKE ?",
+        (searchTerm, searchTerm.replace(" ", "."), "movie%"),
     )
     rows = cursor.fetchall()
-
 
     return rows
 
 
-def clean_data(data):
+def clean_data(data, filters):
     """
     Cleans the data obtained from the database and converts it into a dictionary.
 
     Args:
         data (list): A list of tuples containing film data from the database.
+        filters (dict): A dictionary of the filters that the user would like to filter the results by, e.g. resolution:
+        {filtername: boolean, ...}
 
     Returns:
         dict: A dictionary where the ID of each tuple is the key, and the value is a tuple
@@ -53,27 +58,37 @@ def clean_data(data):
         #                 2: ('Film B', 2021, 'Drama')}
     """
 
-    # test input is for the search "blade.runner"
     cleanedData = {}
     for item in data:
         id = item[0]
         contents = list(item)
         contents.remove(id)
 
+        if contents[4] == None:
+            continue
+
+        if contents[3].lower() == "xxx":
+            continue
+
+        if filters["show-only-4k"] == True:
+            if "4k" not in contents[3]:
+                continue
+        if filters["show-720p"] == False:
+            if "720" in contents[3]:
+                continue
+        if filters["show-480p"] == False:
+            if "480" in contents[3] or "480" in contents[1]:
+                continue
+        if filters["show-xvid-format"] == False:
+            if "xvid" in contents[3]:
+                continue
+
         # size in gb
         contents[4] /= 1073741824
         contents[4] = round(contents[4], 2)
 
-        # # removes 720p films
-        # if "720" in contents[3]:
-        #     continue
-
-    
-
         cleanedData[id] = tuple(contents[:-2])
     return cleanedData
-
-
 
 
 def magnetiser(hash, name):
@@ -82,9 +97,8 @@ def magnetiser(hash, name):
 
 def main():
     cursor, conn = init_db("rarbg_db.sqlite")
-    print(get_filter_parameters())
     searchTerm = input("Enter a search term: ")
-    data = clean_data(fetch_data(searchTerm, cursor))
+    data = clean_data(fetch_data(searchTerm, cursor), get_filter_parameters())
     for item in data:
         print(data[item])
 
